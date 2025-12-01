b77b73eff9a4ff784733ffc7b352f77938d4af63
legleb <sedovgleb2570@gmail.com>
2025-12-01 17:16:53 +0300

    add class Dot

--- main.cpp
+++ main.cpp
@@ -1,27 +1,53 @@
 #include <iostream>
 #include <cstddef>
 
-namespace top {
-  struct p_t {
+namespace top
+{
+  struct p_t
+  {
     int x, y;
   };
 
-  struct IDraw {
-    virtual p_t begin() = 0;
-    virtual p_t next(p_t) = 0;
+  struct IDraw
+  {
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
   };
 
-  bool operator==(p_t a, p_t b) {
+  bool operator==(p_t a, p_t b)
+  {
     return a.x == b.x && a.y == b.y;
   }
 
-  bool operator!=(p_t a, p_t b) {
+  bool operator!=(p_t a, p_t b)
+  {
     return !(a == b);
+  }
+
+  struct Dot: IDraw
+  {
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
   }
 }
 
 int main()
 {
-  using namespace top;
-  
+  using namespece top;
+}
+
+top::Dot::Dot(int x, int y):
+IDraw(), o{x, y}
+{}
+
+top::p_t top::Dot::begin() const
+{
+  return o;
+}
+
+top::p_t top::Dot::next(p_t) const
+{
+  return begin();
 }
