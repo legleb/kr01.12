06c8a794ddc190088cc0d11b28975d4d0c78edcb
legleb <sedovgleb2570@gmail.com>
2025-12-25 15:21:55 +0300

    cli with strs

--- cli.cpp
+++ cli.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <iostream>
 #include <cstddef>
 
@@ -6,19 +7,32 @@ void hi()
   std::cout << "<HI!>\n";
 }
 
+bool is_space(char c)
+{
+  return std::isspace(c);
+}
+
+std::istream & getword(std::istream & is, char * word, size_t k, bool(*c)(char));
+
+size_t match(const char * word, const char * const * words, size_t k);
+
 int main()
 {
   constexpr size_t cmds_count = 1;
   void(*cmds[1])() = {hi};
-  size_t i = 0;
-  while (!(std::cin >> i).eof())
+  const char * const cmds_text[] = {"hi"};
+
+  constexpr size_t bsize = 255;
+  char word[bsize + 1] = {};
+
+  while (!(getword(std::cin, word, bsize, is_space).eof()))
   {
     if (std::cin.fail())
     {
       std::cerr << "< INVALID COMMAND >\n";
       return 1;
     }
-    else if (i < cmds_count)
+    else if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count)
     {
       cmds[i]();
     }
