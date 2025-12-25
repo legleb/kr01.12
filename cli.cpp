f02d2aa4080b8fe35cbd98724c414ab61bc2514a
legleb <sedovgleb2570@gmail.com>
2025-12-25 16:09:08 +0300

    cli.cpp with hi and hello

--- cli.cpp
+++ cli.cpp
@@ -1,4 +1,5 @@
 #include <cctype>
+#include <cstring>
 #include <cassert>
 #include <iostream>
 #include <cstddef>
@@ -10,12 +11,17 @@ void hi()
   std::cout << "<HI!>\n";
 }
 
+void hello()
+{
+  std::cout << "<HELLO!>\n";
+}
+
 bool is_space(char c)
 {
   return std::isspace(c);
 }
 
-std::istream & getword(std::istream & is, char * word, size_t k, bool(*c)(char))
+std::istream & getword(std::istream & is, char * word, size_t k, size_t & size, bool(*c)(char))
 {
   assert(k > 0 && "k must be greater than 0");
   if (!k || !word)
@@ -29,34 +35,52 @@ std::istream & getword(std::istream & is, char * word, size_t k, bool(*c)(char))
     is >> next;
     word[i] = next;
   }
+  size = i;
+  word[i] = '\0';
   return is >> std::skipws;
 }
 
-size_t match(const char * word, const char * const * words, size_t k);
+size_t match(const char * word, const char * const * words, size_t k)
+{
+  for (size_t i = 0; i < k; ++i)
+  {
+    if (std::strlen(word) == std::strlen(words[i]) && !std::strcmp(word, words[i]))
+    {
+      assert(i < k && "i must be less than k");
+      return i;
+    }
+  }
+  return k;
+}
 
 int main()
 {
-  constexpr size_t cmds_count = 1;
-  void(*cmds[1])() = {hi};
-  const char * const cmds_text[] = {"hi"};
+  constexpr size_t cmds_count = 2;
+  void(*cmds[2])() = {hi, hello};
+  const char * const cmds_text[] = {"hi", "hello"};
 
-  constexpr size_t bsize = 255;
-  char word[bsize + 1] = {};
+  constexpr size_t bcapacity = 255;
+  char word[bcapacity + 1] = {};
+  size_t size = 0;
 
-  while (!(getword(std::cin, word, bsize, is_space).eof()))
+  while (!(getword(std::cin, word, bcapacity, size, is_space).eof()))
   {
     if (std::cin.fail())
     {
       std::cerr << "< INVALID COMMAND >\n";
       return 1;
     }
-    else if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count)
-    {
-      cmds[i]();
-    }
     else
     {
-      std::cerr << "< UNKNOWN COMMAND >\n";
+      word[size - 1] = '\0';
+      if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count)
+      {
+        cmds[i]();
+      }
+      else
+      {
+        std::cerr << "< UNKNOWN COMMAND >\n";
+      }
     }
   }
 }
